9b6c38fb419c75d42f02b77cb9162ced67c28802
0xADADA <0xadada.pub@0xadada.pub>
2017-10-27 07:14:27 -0400

    Global abatement of `window.history` in tests

--- tests/unit/services/window-test.js
+++ tests/unit/services/window-test.js
@@ -10,37 +10,41 @@ moduleFor('service:window', 'Unit | Service | window', {
     // reset location hash
     window.location.hash = '';
   },
+  afterEach() {
+    // reset location hash
+    window.location.hash = '';
+  },
 });
 
 test('it can call history.back and navigate back', function(assert) {
+  let done = assert.async();
+
   // navigate forwards, manually
   window.location.hash = '#back';
   window.location.hash = '#forward';
   let service = this.subject();
   assert.ok(service, 'service is ok');
   // navigate back
-  get(service, 'history.back')();
+  get(service, 'history.back')(window);
   later(this, () => {
     assert.equal(window.location.hash, '#back', '`history.back` was called');
+    done();
   }, 250);
 });
 
 test('it can call history.back if `history` is not available (e.g. in FastBoot)', function(assert) {
+  let done = assert.async();
+
   // navigate forwards, manually
-  window.location.hash = '#same';
+  let hash = window.location.hash;
   let service = this.subject();
   assert.ok(service, 'service is ok');
 
-  // cache history
-  let cacheHistory = window.history;
-  delete window.history;
-
   // navigate back
-  get(service, 'history.back')();
+  get(service, 'history.back')({});
   later(this, () => {
-    assert.equal(window.location.hash, '#same', '`history.back` was not called');
+    assert.equal(window.location.hash, hash, '`history.back` was not called');
+    // restore history
+    done();
   }, 250);
-
-  // restore history
-  window.history = cacheHistory;
 });
